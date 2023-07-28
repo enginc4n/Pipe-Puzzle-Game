7d391561c4b1042f033254112469b49f1c89f8a9
Engin Can Altınöz <altinozengincan@gmail.com>
2023-07-28 23:11:24 +0300

    Empty Unity Project

---
+++ Pipe Puzzle Game/Assets/Script/Runtime/Context/Game/Scripts/Config/GameBootstrap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using strange.extensions.context.impl;
+
+namespace Script.Runtime.Context.Game.Scripts.Config
+{
+  public class GameBootstrap : ContextView
+  {
+    private void Awake()
+    {
+      context = new GameContext(this);
+    }
+  }
+}
---
+++ Pipe Puzzle Game/Assets/Script/Runtime/Context/Game/Scripts/Config/GameContext.cs
@@ -0,0 +1,22 @@
+﻿using strange.extensions.context.api;
+using strange.extensions.context.impl;
+using UnityEngine;
+
+namespace Script.Runtime.Context.Game.Scripts.Config
+{
+  public class GameContext: MVCSContext
+  {
+    public GameContext(MonoBehaviour view) : base(view)
+    {
+    }
+
+    public GameContext(MonoBehaviour view, ContextStartupFlags flags) : base(view, flags)
+    {
+    }
+
+    protected override void mapBindings()
+    {
+      base.mapBindings();
+    }
+  }
+}
